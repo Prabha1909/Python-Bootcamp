d58426b9ea6105e600145dba16f4fa077b2a78c5
Prabha1909 <prabha.svm@gmail.com>
2018-10-20 19:36:22 +0530

    String indexing

--- Objects and data types/Strings.py
+++ Objects and data types/Strings.py
@@ -2,5 +2,11 @@
 print("Hello World")
 
 #to find the length of a string
-a = "Hello World"
-print(len(a))
+s = "Hello World"
+print(len(s))
+
+# string indexing
+print(s[0])
+print(s[-1])
+print(s[2])
+print(s[-3])
